62f71ea8c80ba9fb53b14ae9c8743db96d25c241
physicskdw <physicskdw@gmail.com>
2026-04-26 20:38:02 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'munsu2-cache-v1';
+const urlsToCache = [
+  '/munsu2/',
+  '/munsu2/index.html',
+  '/munsu2/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
